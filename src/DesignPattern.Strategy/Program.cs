1e05df9ab3a03df2c4273bb5dd5b963e4afe45c6
jcsnorlax97 <justin.choi.developer@gmail.com>
2021-10-04 08:16:20 -0500

    Init Commit & Repo Setup - new Solution; new Project; Hello World; gitignore; editorconfig

---
+++ src/DesignPattern.Strategy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPattern.Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
